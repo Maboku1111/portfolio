9bc1dcfcee73ad9d5ee51c8d7a5aba23ab574644
Maboku1111 <mabokuseimela6@gmail.com>
2025-03-18 16:31:24 +0200

    gsap, still in progress

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -2,7 +2,6 @@
 export async function load({ fetch, params }) {
 	const res = await fetch(`https://bible-api.com/data/web/random`)
 	const verse = await res.json();
-	console.log(verse.random_verse.book + " " + verse.random_verse.chapter + ":" + verse.random_verse.verse)
 
 	return { verse }
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,34 @@
 <script>
+    import { gsap } from "gsap";
+    import SplitText from "gsap";
+    import { onMount } from "svelte";
+
     /** @type {import('./$types').PageProps} */
 	let { data } = $props();
-    console.log(data)
+    gsap.registerPlugin(SplitText)
+
+    onMount(() => {
+        var tl = gsap.timeline()
+        mySplitText = new SplitText("#heading", { type: "words, chars"})
+        chars = mySplitText.chars
+        
+        gsap.set("#heading", { perspective: 400})
+
+        console.log(chars)
+
+        tl.from(chars, {
+            duration: 0.8,
+            opacity: 0,
+            scale: 0,
+            y: 80,
+            rotationX: 180,
+            transformOrigin: "0% 50% -50",
+            ease: "back",
+            stagger: 0.01
+        });
+    })
+
+    export const layout = null;
 </script>
 
-<h1>{data.verse.random_verse.book + " " + data.verse.random_verse.chapter + ":" + data.verse.random_verse.verse}</h1>
+<div id="heading">{data.verse.random_verse.book + " " + data.verse.random_verse.chapter + ":" + data.verse.random_verse.verse}</div>
